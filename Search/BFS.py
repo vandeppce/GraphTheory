a6b75fdafaf83e547d59780a0ce51c92630314c7
vandeppce <sdtczyj@163.com>
2018-01-31 16:57:55 +0800

    2017/1/31

--- Search/BFS.py
+++ Search/BFS.py
@@ -42,18 +42,18 @@ for i in str:
 node['a'] = 1
 
 node_queue = queue.Queue(0)
-node_queue.put('a')
+node_queue.put('a')       #从a开始遍历
 
 list = []
 
-while node_queue.qsize() != 0:
-    tmp = trans[node_queue.get()]
+while node_queue.qsize() != 0:      #终止条件，队列为空
+    tmp = trans[node_queue.get()]   #返回队列头元素
     list.append(retrans[tmp])
     for j in range(9):
-        if G[tmp][j] == 1:
-            if node[retrans[j]] == 0:
-                node[retrans[j]] = 1
-                node_queue.put(retrans[j])
+        if G[tmp][j] == 1:          #查找所有和该元素构成边都顶点
+            if node[retrans[j]] == 0:   #如果该顶点未遍历
+                node[retrans[j]] = 1    #打上标记
+                node_queue.put(retrans[j])  #送入队列
 
 print(list)
 
